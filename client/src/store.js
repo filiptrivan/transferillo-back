27fd4f61124f205fd511834636d759fd21c0a4ae
filiptrivan <120225885+filiptrivan@users.noreply.github.com>
2023-03-06 18:48:06 +0100

    izbrisao producition

--- client/src/store.js
+++ client/src/store.js
@@ -1,5 +1,5 @@
 import { createStore, applyMiddleware } from "redux";
-import { composeWithDevTools } from "redux-devtools-extension";
+// import { composeWithDevTools } from "redux-devtools-extension";
 import thunk from "redux-thunk";
 import rootReducer from "./reducers";
 import setAuthToken from "./utils/setAuthToken";
@@ -11,7 +11,7 @@ const middleware = [thunk];
 const store = createStore(
   rootReducer,
   initialState,
-  composeWithDevTools(applyMiddleware(...middleware))
+  applyMiddleware(...middleware)
 );
 
 /*
